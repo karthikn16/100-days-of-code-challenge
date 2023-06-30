0e9122d0badd689a3741124eb5d9987c358507a6
karthikeyan <81953615+karthikn16@users.noreply.github.com>
2023-06-30 20:52:47 +0530

    day 3

---
+++ day3/day3.js
@@ -0,0 +1,41 @@
+var age=15
+if (age >= 18){
+    console.log("you can register")
+}
+else{
+    console.log("sorry")
+}
+
+
+
+
+var age = personage >=18 ? console.log("register"):console.log("sorry")
+
+
+
+
+var number = 10;
+function multiplier ( whichnumber){
+    return function(x){
+        return x*whichnumber
+    }
+}
+
+
+
+count = 0;
+while (count <3){
+    console.log("hi")
+    count++
+}
+
+
+add =()=> {
+    return(2+2)
+}
+
+
+
+
+
+add1 = (number) => (number+3)
