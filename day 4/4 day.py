67c4d9103de12a49785105418118840f83b1c4ab
karthikeyan <81953615+karthikn16@users.noreply.github.com>
2023-07-07 13:01:50 +0530

    Add files via upload

---
+++ day 4/4 day.py
@@ -0,0 +1,22 @@
+
+
+
+using nested if
+num = 15
+if num>=0:
+    if num==0:
+        print('Zero')
+    else:
+        print("Positive")
+else:
+    print("Negative")
+
+using brute force  
+num = 15
+if num>=0:
+    if num==0:
+        print('Zero')
+    else:
+        print("Positive")
+else:
+    print("Negative")
